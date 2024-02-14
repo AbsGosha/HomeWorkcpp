f9c31ce89bc023a73781c25a8d1ba1d5a9f5ac11
AbsGosha <159914520+AbsGosha@users.noreply.github.com>
2024-02-14 13:39:19 +0300

    Add files via upload

---
+++ 10.02.2024DZ.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ // задание 1
+	setlocale(LC_ALL, "rus");
+	cout << "Автор:\t"; cout << "А.С.Пушкин\n\n";
+	cout << "У лукоморья дуб зеленый\n";
+	cout << "Златая цепь на дубе том\n";
+	cout << "И днем и ночью кот ученый\n";
+	cout << "Все ходит по цепи кругом\n\n";
+// задание 2
+	cout << "Каждый\n";
+	cout << "\tОхотник\n";
+	cout << "\t\tЖелает\n";
+	cout << "\t\t\tЗнать\n";
+	cout << "\t\t\t\tГде\n";
+	cout << "\t\t\t\t\tСидит\n";
+	cout << "\t\t\t\t\t\tФазан\n\n";
+// задание 3 
+	cout << "\t\t\t\t\tПродаю кошку\n";
+	cout << "\t\t\t\t\tПорода кошки: Британская\n";
+	cout << "\t\t\t\t\tТелефон: 123-45-67\n";
+	cout << "\t\t\t\t\t891\t|01-23|\n";
+	cout << "\t\t\t\t\t456\t|78-91|\n";
+	cout << "\t\t\t\t\t012\t|34-56|\n";
+
+
+
+}
