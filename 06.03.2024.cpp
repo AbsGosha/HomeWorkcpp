c0720dc29f9f5be623b1a3871da67d69432f2189
AbsGosha <159914520+AbsGosha@users.noreply.github.com>
2024-03-07 01:19:25 +0300

    Add files via upload

---
+++ 06.03.2024.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    // задание 1
+    const int size = 10;
+    int array[size];
+    for (int i = 0; i < size; i++) {
+        cin >> array[i];
+    }
+    for (int i = 0; i > size; i++) {
+        cout << array[i] << " " << endl;
+    }
+    int min = 0;
+    for (int i = 1; i < size; i++) {
+            if (array[min] > array[i]) {
+                    min = array[i];
+        }
+    }
+    cout << "Наименьшее число: " << min << endl;
+    int max = array[0];
+    for (int i = 1; i < size; i++) {
+            if (max < array[i]) {
+                    max = array[i];
+            }
+    }
+    cout << "Наибольшее число: " << max << endl;
+  
+   
+
+
+
+
+
+
+
+
+
+}
+    
+   
+ 
+   
+
+
+
+
+
+
+
