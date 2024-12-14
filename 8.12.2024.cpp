c89104e74de2c9a268990a5e16f195236c8fc04d
AbsGosha <159914520+AbsGosha@users.noreply.github.com>
2024-12-14 14:35:22 +0300

    Add files via upload

---
+++ 8.12.2024.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+#include "BinaryTree.h"
+int main()
+{
+	int* arr = new int[5] { 5,3,7,8,4 };
+	BinaryTree tree(arr, 5);
+	tree.print(tree.getRoot());
+	cout << endl;
+	tree.insert(4, tree.getRoot());
+	cout << endl;
+	tree.print(tree.getRoot());
+}
